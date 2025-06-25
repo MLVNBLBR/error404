8ddb8fc77922c808c7d3046cec5380f71b8c177b
hetses <22-79953@g.batstate-u.edu.ph>
2025-06-25 17:16:11 +0800

    Defined a function on main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ document.getElementById("title").innerText = "Welcome";
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
 // toggleMenu is not defined
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    if (menu) {
+        menu.style.display = (menu.style.display === "none" || !menu.style.display) ? "block" : "none";
+    }
+}

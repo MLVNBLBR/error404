54b2bfedbcd14233e0832653a9dc6a4dd68c4b7f
iamkian <22-77050@g.batstate-u.edu.ph>
2025-06-25 17:51:47 +0800

    I use const or let instead of var.

--- js/form.js
+++ js/form.js
@@ -1,7 +1,8 @@
 // Buggy form.js
+// Buggy form.js
 document.querySelector("form").addEventListener("submit", function(event) {
-    var name = document.querySelector("[name='name']").value;
-    var email = document.querySelector("[name='email']").value;
+    const name = document.querySelector("[name='name']").value;
+    const email = document.querySelector("[name='email']").value;
 
     if (email.indexOf('@') === -1) {
         alert("Invalid email");
@@ -10,9 +11,9 @@ document.querySelector("form").addEventListener("submit", function(event) {
     alert("Form submitted!");
 });
 
-var submitButton = document.querySelector("button");
+const submitButton = document.querySelector("button");
 submitButton.addEventListener("click", function() {
-    var name = document.querySelector("[name='name']").value;
+    const name = document.querySelector("[name='name']").value;
     if (name === "") {
         alert("Name is required");
     }

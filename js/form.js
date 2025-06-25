b7fd501fea142c33b330aa7e0acd67ab01afaeb9
hetses <22-79953@g.batstate-u.edu.ph>
2025-06-25 17:23:14 +0800

    changed var into const

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
 // Buggy form.js
 document.querySelector("form").addEventListener("submit", function(event) {
-    var name = document.querySelector("[name='name']").value;
-    var email = document.querySelector("[name='email']").value;
+    const name = document.querySelector("[name='name']").value;
+    const email = document.querySelector("[name='email']").value;
 
     if (email.indexOf('@') === -1) {
         alert("Invalid email");
@@ -10,9 +10,9 @@ document.querySelector("form").addEventListener("submit", function(event) {
     alert("Form submitted!");
 });
 
-var submitButton = document.querySelector("button");
+const submitButton = document.querySelector("button");
 submitButton.addEventListener("click", function() {
-    var name = document.querySelector("[name='name']").value;
+    const name = document.querySelector("[name='name']").value;
     if (name === "") {
         alert("Name is required");
     }
